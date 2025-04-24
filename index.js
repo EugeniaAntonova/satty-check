e3627e119ecfaa46d6ed8b34039713b575f2d6af
--replace-all <alexocean.e.k@gmail.com>
2025-04-24 16:50:22 +0500

    feat basic sections and animation

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 "use strict";
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   new Scroller(".js-scroller-hero");
-//   new Scroller(".js-scroller-prizes");
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  new AccordeonControl(".accordeon");
+  //   new Scroller(".js-scroller-hero");
+  //   new Scroller(".js-scroller-prizes");
+});
 
 class Scroller {
-    constructor(selector) {
-      this.scroller = document.querySelector(selector);
-      this.scroller.addEventListener("click", this.onClick.bind(this));
-      this.anchor = this.scroller.dataset.target;
-    }
-  
-    onClick() {
-      document
-        .querySelector(`#${this.anchor}`)
-        .scrollIntoView({ block: "start", behavior: "smooth" });
-    }
+  constructor(selector) {
+    this.scroller = document.querySelector(selector);
+    this.scroller.addEventListener("click", this.onClick.bind(this));
+    this.anchor = this.scroller.dataset.target;
   }
+
+  onClick() {
+    document
+      .querySelector(`#${this.anchor}`)
+      .scrollIntoView({ block: "start", behavior: "smooth" });
+  }
+}
+
+class AccordeonControl {
+  constructor(selector) {
+    this.accordeon = document.querySelector(selector);
+    this.active = null;
+    this.accordeon.addEventListener("click", this.onAccordeonClick.bind(this));
+    this.accordeon.addEventListener("keydown", this.onSpace.bind(this));
+  }
+
+  onAccordeonClick(evt) {
+    let target = evt.target;
+    if (!(target.tagName == "SUMMARY" || target == this.active)) return;
+    if (this.active) {
+      this.active.closest("details").removeAttribute("open");
+    }
+    this.active = target;
+  }
+
+  onSpace(evt) {
+    if (evt.code != "Space") return;
+    if (evt.target != this.active) return;
+    this.active.removeAttribute("open");
+    this.active = null;
+  }
+}
